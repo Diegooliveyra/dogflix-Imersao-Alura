ab2b47faf5990d4da48459172b8ecd1391e674ea
Diego Oliveira <54894831+Diegooliveyra@users.noreply.github.com>
2020-07-31 13:54:51 -0300

    Finalizado primeiro dia da imersão

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Logo from '../../acess/image/Logo.png';
 import './Menu.css';
+import Button from '../Button';
+//import ButtonLink from './components/ButttonLink';
 
-function Menu () {
+function Menu() {
     return (
         <nav className="Menu">
             <a href="/">
-                <img className='Logo' src={Logo} alt = 'DogFlix Logo'/>
+                <img className='Logo' src={Logo} alt='DogFlix Logo' />
             </a>
-            <a className="ButtonLink" href="/">
+            <Button as='a' className="ButtonLink" href="/">
                 Novo Video
-            </a>
+            </Button>
         </nav>
     );
 }

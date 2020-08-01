9b7313d83fa48646ff04c7f5b5b8f982c2f7be18
Diego Oliveira <diegooliveirabryto@gmail.com>
2020-08-01 09:47:33 -0300

    Finalizado o roteamento

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import Logo from '../../acess/image/Logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -7,10 +8,10 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className='Logo' src={Logo} alt='DogFlix Logo' />
-            </a>
-            <Button as='a' className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link}className="ButtonLink" to="/casdastro/Video">
                 Novo Video
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/casdastro/Video" component={CadastroVideo}/> 
+      <Route path="/cadastro/categoria" component={CadastroCategoria}/> 
+      <Route component={()=>(<div>Pagina 404</div>)} exact/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 

682f7dd432ad2408cbd9a9720bfc4d570b39365f
Diego Oliveira <diegooliveirabryto@gmail.com>
2020-08-07 21:52:45 -0300

    Update PageDefault

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,24 +1,26 @@
-import React from "react";
-import Footer from "../Footer";
-import Menu from "../Menu";
-import styled from "styled-components";
+import React from 'react';
+import styled, { css } from 'styled-components';
+import Footer from '../Footer';
+import Menu from '../Menu';
 
 const Main = styled.main`
   background-color: var(--black);
   color: var(--white);
-  flex:1;
+  flex: 1;
   padding-top: 50px;
   padding-left: 5%;
   padding-right: 5%;
-
+  ${({ paddingAll }) => css`
+    padding: ${paddingAll};
+  `}
 `;
 
-function PageDefault({ children }) {
+function PageDefault({ children, paddingAll }) {
   return (
     <>
       <Menu />
-       <Main>
-        {children}
+      <Main paddingAll={paddingAll}>
+          {children}
         </Main>
       <Footer />
     </>
